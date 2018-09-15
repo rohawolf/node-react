99e1b9d6a0f362732c23b2a7e4842085e5ac87d6
rohawolf <rohawolf@gmail.com>
2018-09-16 01:30:19 +0900

    server.js refactoring

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,7 @@ const api_server = express();
 api_server.use(bodyParser.json());
 api_server.use(bodyParser.urlencoded({ extended: true }));
 
-let users = [
-    {
-        id: 1,
-        name: 'roha',
-    },
-    {
-        id: 2,
-        name: 'eunjin',
-    },
-]
+api_server.use('/users', require('./modules/users'));
 
 api_server.get('/', (req, res) => {
     res.send('Hello World!\n');
@@ -22,64 +13,6 @@ api_server.get('/', (req, res) => {
 
 api_server.get('/status', (req, res) => {
     res.send('OK\n');
-});
-
-// routers for users
-api_server.get('/users', (req, res) => {
-    return res.json(users);
-});
-
-api_server.get('/users/:id', (req, res) => {
-    // validate request parameter
-    const id = parseInt(req.params.id, 10);
-    if (!id) {
-        return res.status(400).json({ error: 'Incorrect Id' });
-    }
-
-    // find user by id & return
-    let user = users.filter( user => user.id === id)[0];
-    if (!user) {
-        return res.status(404).json({ error: 'Unknown user'});
-    }
-    return res.json(user);
-});
-
-api_server.delete('/users/:id', (req, res) => {
-    // validate request parameter
-    const id = parseInt(req.params.id, 10);
-    if (!id) {
-        return res.status(400).json({ error: 'Incorrect Id' });
-    }
-
-    // find user's index to delete by id
-    const userIdx = users.findIndex( user => user.id === id);
-    if (userIdx === -1) {
-        return res.status(404).json({ error: 'Unknown user'});
-    }
-
-    users.splice(userIdx, 1);
-    res.status(204).send();
-});
-
-api_server.post('/users', (req, res) => {
-    // validate request parameter
-    const name = req.body.name || '';
-    if (!name.length) {
-        return res.status(400).json({ error: 'Incorrect name'});
-    }
-
-    const id = users.reduce((maxId, user) => {
-        return user.id > maxId ? user.id : maxId;
-    }, 0) + 1;
-
-    // create new user with id, name
-    const newUser = {
-        id: id,
-        name: name
-    }
-    users.push(newUser);
-    return res.status(201).json(newUser);
-
 });
 
 api_server.listen(3000, () => {
